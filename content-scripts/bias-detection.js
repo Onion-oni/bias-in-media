0705c0230cba4ff3c1427590e8e1b8d0e2377b3d
cynthiaqian <cinderfly@gmail.com>
2025-07-16 19:17:13 -0700

    adding basic functionality of printing all visible text to console and highlighting text on webpage

--- content-scripts/bias-detection.js
+++ content-scripts/bias-detection.js
@@ -5,14 +5,59 @@
 // Function to inject CSS
 
 // Function to gather visible text from the page
-function gatherPageText() {}
+function gatherPageText() {
+    const walker = document.createTreeWalker(
+        document.body,
+        NodeFilter.SHOW_TEXT,
+        {
+            acceptNode: function(node) {
+
+                // skip the text if its detached (no parent), has an invisible parent element, or is whitespace/empty
+                if (!node.parentElement || !node.parentElement.offsetParent || !node.nodeValue.trim()) {
+                        return NodeFilter.FILTER_REJECT;
+                }
+                return NodeFilter.FILTER_ACCEPT;
+            }
+        }
+    );
+
+    const visibleTextNodes = [];
+
+    while (walker.nextNode()) {
+        visibleTextNodes.push(walker.currentNode);
+    }
+
+    return visibleTextNodes;
+}
 
 // Function to detect bias using the TensorFlow.js model
 function detectBias() {}
 
 // Function to highlight biased text based on the results from the model
-function highlightBiasedText() {}
+function addHighlightWrapToBiasedText() {
+    const span = document.createElement('span');
+    span.className = 'bias-span highlighted';
+    span.textContent = textNode.nodeValue;
 
-async function main() {}
+    // replaces original text node with highlighted span and highlights on page
+    textNode.parentNode.replaceChild(span, textNode);
+
+    return span;
+}
+
+//TODO: async when tensorflowmodel is added
+function main() {
+    const textNodes = gatherPageText();
+    const highlightedBias = [];
+
+    //TODO: currently set to highlight all text on page. Set up with model!
+    for (const node of textNodes) {
+        const span = addHighlightWrapToBiasedText(node);
+        highlightedBias.push(span);
+    }
+
+    const fullPageText = highlightedBias.map(s => s.textContent).join (' ');
+    console.log("Extracted Visible Page Text: \n", fullPageText);
+}
 
 main();
